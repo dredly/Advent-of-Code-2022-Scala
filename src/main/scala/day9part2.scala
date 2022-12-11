8c48d07bc4982888d861469f40cb8f9012efafa9
Daniel Redly <dredly@gmail.com>
2022-12-11 00:43:14 +0000

    Day 9 part 2

--- src/main/scala/day9part2.scala
+++ src/main/scala/day9part2.scala
@@ -1,37 +1,68 @@
 import day9.{DOWN, Direction, Instruction, LEFT, Point, RIGHT, UP, parseInput}
-
 import scala.annotation.tailrec
 
 object day9part2 {
-  def moveHead(startingPosition: Point, direction: Direction): Point = {
+
+  case class Move(from: Point, to: Point)
+
+  def moveHead(startingPosition: Point, direction: Direction): Move = {
     direction match
-      case UP => startingPosition.copy(y = startingPosition.y + 1)
-      case DOWN => startingPosition.copy(y = startingPosition.y - 1)
-      case LEFT => startingPosition.copy(x = startingPosition.x - 1)
-      case RIGHT => startingPosition.copy(x = startingPosition.x + 1)
+      case UP => Move(startingPosition, startingPosition.copy(y = startingPosition.y + 1))
+      case DOWN => Move(startingPosition ,startingPosition.copy(y = startingPosition.y - 1))
+      case LEFT => Move(startingPosition ,startingPosition.copy(x = startingPosition.x - 1))
+      case RIGHT => Move(startingPosition, startingPosition.copy(x = startingPosition.x + 1))
   }
 
-  def moveTail(headPosition: Point, tailPosition: Point, direction: Direction): Point = {
-    val sameDirection = (direction == UP & headPosition.y > tailPosition.y)
-      | (direction == DOWN & headPosition.y < tailPosition.y)
-      | (direction == LEFT & headPosition.x < tailPosition.x)
-      | (direction == RIGHT & headPosition.x > tailPosition.x)
+  def takeAvgPlace(start: Point, end: Point): Point = {
+    Point(x = (start.x + end.x) / 2, y = (start.y + end.y) / 2)
+  }
 
-    if sameDirection then headPosition else tailPosition
+  def isDirectlyNeighbouring(point1: Point, point2: Point): Boolean = {
+    ((point1.x - point2.x).abs == 1 & point1.y == point2.y) | ((point1.y - point2.y).abs == 1 & point1.x == point2.x)
+  }
+
+  def isDiagonallyNeighbouring(point1: Point, point2: Point): Boolean = {
+    (point1.x - point2.x).abs == 1 & (point1.y - point2.y).abs == 1
+  }
+
+  def isNeighbouring(point1: Point, point2: Point): Boolean = {
+    isDirectlyNeighbouring(point1, point2) | isDiagonallyNeighbouring(point1, point2)
+  }
+
+  def moveTail(headMove: Move, tailPosition: Point): Move = {
+    val onTop = headMove.from == tailPosition | headMove.to == tailPosition
+    val didHeadMove = headMove.to != headMove.from
+    val noTailMovement = onTop | !didHeadMove | isNeighbouring(headMove.to, tailPosition)
+
+    val isAveragable = (headMove.to.x - tailPosition.x).abs % 2 == 0 & (headMove.to.y - tailPosition.y).abs % 2 == 0
+
+    val followHead = isDiagonallyNeighbouring(headMove.from, tailPosition) & isDirectlyNeighbouring(headMove.from, headMove.to)
+
+    if noTailMovement then Move(tailPosition, tailPosition) else {
+      if isAveragable then Move(tailPosition, takeAvgPlace(tailPosition, headMove.to)) else {
+        if followHead then Move(tailPosition, headMove.from) else {
+          Move(tailPosition, tailPosition.copy(
+            x = tailPosition.x + (headMove.to.x - headMove.from.x),
+            y = tailPosition.y + (headMove.to.y - headMove.from.y)
+          ))
+        }
+      }
+    }
   }
 
   @tailrec
-  def moveSnakeBody(head: Point, body: List[Point], direction: Direction, moved: List[Point] = List()): List[Point] = {
+  def moveSnakeBody(headMove: Move, body: List[Point], direction: Direction, moves: List[Move] = List()): List[Point] = {
     body.length match
-      case 0 => moved
+      case 0 => moves.map(move => move.to)
       case _ =>
-        val newHeadPosition = moveTail(head, body.head, direction)
-        moveSnakeBody(body.head, body.tail, direction, moved :+ newHeadPosition)
+        val newHeadMove = moveTail(headMove, body.head)
+        moveSnakeBody(newHeadMove, body.tail, direction, moves :+ newHeadMove)
   }
 
   def moveWholeSnake(snake: List[Point], direction: Direction): List[Point] = {
-    val movedBody = moveSnakeBody(snake.head, snake.tail, direction)
-    moveHead(snake.head, direction)::movedBody
+    val headMove = moveHead(snake.head, direction)
+    val movedBody = moveSnakeBody(headMove, snake.tail, direction)
+    headMove.to::movedBody
   }
 
   @tailrec
@@ -49,8 +80,6 @@ object day9part2 {
       case 0 => visitedSoFar
       case _ =>
         val instructionResults = doInstruction(instructions.head, visitedSoFar, currentPositions)
-        println(instructionResults)
-        println()
         getVisititedPositionsForTail(instructions.tail, instructionResults(1), visitedSoFar.union(instructionResults(0)))
   }
 
@@ -60,11 +89,6 @@ object day9part2 {
   }
 
   def main(args: Array[String]): Unit = {
-//    val points = List(Point(3, 0), Point(2, 0), Point(1, 0), Point(0, 0))
-//    val result = doInstruction(Instruction(RIGHT, 4), Set(Point(0, 0)), points)
-//    val result = moveWholeSnake(points, RIGHT)
-//    val result = moveTail(Point(1, 0), Point(0, 0), RIGHT)
-//    println(result)
     val source = scala.io.Source.fromFile("day9.txt")
     val input = try source.mkString finally source.close()
     println(s"Part 2 answer: ${totalVisitedByTail(input, 10)}")
